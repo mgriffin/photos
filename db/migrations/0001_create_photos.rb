1eab9a44fcd0db123299b809c98aec2b7a923c69
Mike Griffin <github@mikegriffin.ie>
2020-05-12 23:53:12 +0100

    Adds a photos model migration

---
+++ db/migrations/0001_create_photos.rb
@@ -0,0 +1,14 @@
+Sequel.migration do
+  up do
+    create_table(:photos) do
+      primary_key :id
+      String :filename, null: false, unique: true
+      String :title
+      DateTime :created_at
+    end
+  end
+
+  down do
+    drop_table(:photos)
+  end
+end
