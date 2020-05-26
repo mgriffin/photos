a6505f8ac1ebeacc2132ba6fb8669e14d05fce38
Mike Griffin <github@mikegriffin.ie>
2020-05-26 23:49:03 +0100

    Adds required unicorn files

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/home/photos/photos"
+pid "/home/photos/photos/tmp/pids/unicorn.pid"
+stderr_path "/home/photos/photos/logs/unicorn.log"
+stdout_path "/home/photos/photos/logs/unicorn.log"
+
+listen "/tmp/unicorn.photos.sock"
+worker_processes 2
+timeout 30
