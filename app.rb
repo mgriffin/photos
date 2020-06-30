d7432f75be6834abee0550ab398e8d4a5d068cbf
Mike Griffin <github@mikegriffin.ie>
2020-06-30 23:42:24 +0100

    Uses Fog to show images

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'dotenv'
+require 'fog/aws'
 require 'sequel'
 require 'sinatra'
 
--- lib/models/photo.rb
+++ lib/models/photo.rb
@@ -1,2 +1,17 @@
+# frozen_string_literal: true
+
 class Photo < Sequel::Model
+  def aws
+    @aws ||= Fog::Storage.new({
+                                provider: 'AWS',
+                                aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+                                aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+                                region: ENV['AWS_REGION']
+                              })
+  end
+
+  def url
+    aws.directories.new(key: ENV['AWS_BUCKET'])
+       .files.get_https_url(filename, Time.now.to_i + 10)
+  end
 end
