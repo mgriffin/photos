9853a2fef70f632b23fa5303c3409a2ee4d9a814
Mike Griffin <github@mikegriffin.ie>
2020-07-05 22:54:59 +0100

    Removes the unneeded url function

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,10 @@
-working_directory "/home/photos/photos"
-pid "/home/photos/photos/tmp/pids/unicorn.pid"
-stderr_path "/home/photos/photos/logs/unicorn.log"
-stdout_path "/home/photos/photos/logs/unicorn.log"
+# frozen_string_literal: true
 
-listen "/tmp/unicorn.photos.sock"
+working_directory '/home/photos/photos'
+pid '/home/photos/photos/tmp/pids/unicorn.pid'
+stderr_path '/home/photos/photos/logs/unicorn.log'
+stdout_path '/home/photos/photos/logs/unicorn.log'
+
+listen '/tmp/unicorn.photos.sock'
 worker_processes 2
 timeout 30
